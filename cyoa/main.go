3f5d198cb58b6fe314f2929bee97cc0a403dcdaa
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-10-01 17:09:45 +0200

    Choose Your Own Adventure: improvments.

--- cyoa/main.go
+++ cyoa/main.go
@@ -19,24 +19,57 @@ type Chapter struct {
 	} `json:"options"`
 }
 
-type DefaultHandler struct {}
+type chaptersHandler struct {
+	tpl      *template.Template
+	chapters map[string]Chapter
+}
 
-func (DefaultHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+var defaultTemplate = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		<article>
+	    {{range .Story}}<p>{{.}}</p>{{end}}
+		</article>
+		<h2>What will you do now?</h2>
+		<ul>
+		{{range .Options}}
+			<li>
+			<a href="/{{.Arc}}">{{.Text}}</a>
+			</li>
+		{{else}}<a href="/intro">Restart?</a></li>
+		{{end}}
+		</ul>
+	</body>
+</html>`
+
+func main() {
+	handler := chaptersHandler{
+		tpl:      template.Must(template.New("").Parse(defaultTemplate)),
+		chapters: parseChapters(),
+	}
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", defaultMux(handler))
+}
+
+func (h chaptersHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	//Does it match a known arc?
-	chapters := parseChapters()
 	arc := strings.TrimLeft(req.URL.Path, "/")
 
-	if v, ok := chapters[arc]; ok {
-		t, err := template.New(arc).Parse(chapterTemplate())
-		checkErr(err)
-
-		err = t.Execute(rw, v)
+	if v, ok := h.chapters[arc]; ok {
+		err := h.tpl.Execute(rw, v)
 		checkErr(err)
 		return
 	}
 
 	//Lost in space? Lets try to redirect to intro arc.
-	if _, ok := chapters["intro"]; ok{
+	if _, ok := h.chapters["intro"]; ok {
 		http.Redirect(rw, req, "/intro", http.StatusPermanentRedirect)
 		return
 	}
@@ -63,37 +96,8 @@ func checkErr(err error) {
 	}
 }
 
-func main() {
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", defaultMux())
-}
-
-func defaultMux() *http.ServeMux {
+func defaultMux(handler chaptersHandler) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle("/", DefaultHandler{})
+	mux.Handle("/", handler)
 	return mux
-}
-
-func chapterTemplate() string {
-	return `
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>{{.Title}}</title>
-	</head>
-	<body>
-		<h1>{{.Title}}</h1>
-		<article>{{.Story}}</article>
-		<h2>What will you do now?</h2>
-		<ul>
-		{{range .Options}}
-			<li>
-			<a href="/{{.Arc}}">{{.Text}}</a>
-			</li>
-		{{else}}<a href="/intro">Restart?</a></li>
-		{{end}}
-		</ul>
-	</body>
-</html>`
 }
