a88cb04732184bb329e8a9081030a62598d3676d
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-10-15 20:31:17 +0200

    wip sitemap

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -1,22 +1,44 @@
 package sitemap
 
-import "github.com/jkuma/gophercises/linkparser"
+import (
+	"fmt"
+	"github.com/jkuma/gophercises/linkparser"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
 
 // Add a map that stores every links
 // Create a
 
 type Location struct {
 	Parent string
+	Link linkparser.Link
+	Depth int16
+}
+
+type Sitemap struct {
 	Url string
 	Depth int16
 }
 
-type Map struct {
-	Url string
-	Depth int16
-}
-
-func (s Map) Build() {
+func (s Sitemap) Build() map[string][]Location {
 	var l map[string][]Location
 	c := make(chan []linkparser.Link)
+
+	resp, err := http.Get(s.Url)
+
+	return l
+}
+
+func (s Sitemap) PrintXML() {
+	var w string
+	l := s.Build()
+
+	for url, _ := range l {
+		w = fmt.Sprintf("%v \n", url)
+	}
+
+	_, _ = io.Copy(os.Stdout, strings.NewReader(w))
 }
