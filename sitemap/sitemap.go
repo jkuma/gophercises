9dfcdd7d1f1b3e1450a418e240ac5328b25f529e
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-10-16 14:56:56 +0200

    First draft of sitemap package.

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -4,41 +4,68 @@ import (
 	"fmt"
 	"github.com/jkuma/gophercises/linkparser"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
-// Add a map that stores every links
-// Create a
-
 type Location struct {
 	Parent string
-	Link linkparser.Link
-	Depth int16
+	Link   linkparser.Link
+	Depth  int16
 }
 
 type Sitemap struct {
-	Url string
+	Url   string
 	Depth int16
 }
 
 func (s Sitemap) Build() map[string][]Location {
-	var l map[string][]Location
-	c := make(chan []linkparser.Link)
+	m := make(map[string][]Location)
 
-	resp, err := http.Get(s.Url)
+	if s.Url != "" {
+		c := make(chan []Location)
 
-	return l
+		go locations(s.Url, s.Depth, c)
+		m[s.Url] = <-c
+	}
+
+	return m
 }
 
 func (s Sitemap) PrintXML() {
 	var w string
 	l := s.Build()
 
-	for url, _ := range l {
-		w = fmt.Sprintf("%v \n", url)
+	for url, locs := range l {
+		w += fmt.Sprintf("domain: %v\nlocations: %v", url, locs)
 	}
 
 	_, _ = io.Copy(os.Stdout, strings.NewReader(w))
+}
+
+// Returns a list of locations for the given parent url.
+func locations(parent string, depth int16, c chan []Location) {
+	var locs []Location
+	resp, err := http.Get(parent)
+
+	if err != nil {
+		log.Fatalf("Could not resolve %v, \nerror message: %v", parent, err)
+	}
+
+	l := linkparser.Parser{}.Parse(resp.Body)
+
+	for _, link := range l {
+		locs = append(locs, Location{
+			Parent: parent,
+			Link: linkparser.Link{
+				Href: link.Href,
+				Text: link.Text,
+			},
+			Depth: depth,
+		})
+	}
+
+	c <- locs
 }
