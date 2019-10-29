6e611eab861633c0479f313eb6808c4588a10381
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-10-29 22:27:28 +0100

    Add shuffle method to deck.

---
+++ deck/deck/deck.go
@@ -0,0 +1,44 @@
+package deck
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Deck struct {
+	Cards []Card
+}
+
+type Card struct {
+	Unit  string
+	Color string
+}
+
+func NewDeck() *Deck {
+	var deck Deck
+
+	units := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	colors := []string{"spade", "diamond", "heart", "club"}
+
+	for _, c := range colors {
+		for _, u := range units {
+			deck.Cards = append(deck.Cards, Card{Unit: u, Color: c})
+		}
+	}
+
+	return &deck
+}
+
+func (d *Deck) Shuffle() {
+	var deck Deck
+
+	rand.Seed(time.Now().UnixNano())
+
+	for _ = range d.Cards {
+		i := rand.Intn(len(d.Cards))
+		deck.Cards = append(deck.Cards, d.Cards[i])
+		d.Cards = append(d.Cards[:i], d.Cards[i+1:]...)
+	}
+
+	d.Cards = deck.Cards
+}
---
+++ deck/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	deck2 "github.com/jkuma/gophercises/deck/deck"
+)
+
+func main() {
+	deck := *deck2.NewDeck()
+	deck.Shuffle()
+	fmt.Println(deck)
+}
