219017c20b41fb7ebd4fddd30820bbbae374b5f9
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-11-08 17:16:54 +0100

    Add quiet_hn package.

---
+++ quiet_hn/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/jkuma/gophercises/quiet_hn/hn"
+)
+
+func main() {
+	// parse flags
+	var port, numStories int
+	flag.IntVar(&port, "port", 3000, "the port to start the web server on")
+	flag.IntVar(&numStories, "num_stories", 30, "the number of top stories to display")
+	flag.Parse()
+
+	tpl := template.Must(template.ParseFiles("./index.gohtml"))
+
+	http.HandleFunc("/", handler(numStories, tpl))
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		var client hn.Client
+		ids, err := client.TopItems()
+		if err != nil {
+			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+			return
+		}
+		var stories []item
+
+		c := make(chan item, numStories)
+
+		for k, id := range ids {
+			if k < numStories {
+				go getStoryItem(client, id, c)
+			}
+		}
+
+		for i := 0; i < numStories; i++{
+			stories = append(stories, <-c)
+		}
+
+		data := templateData{
+			Stories: stories,
+			Time:    time.Now().Sub(start),
+		}
+		err = tpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Failed to process the template", http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func getStoryItem(client hn.Client, id int, c chan item) {
+	hnItem, err := client.GetItem(id)
+	if err == nil {
+		item := parseHNItem(hnItem)
+		if isStoryLink(item) {
+			c <- item
+		}
+	}
+}
+
+func isStoryLink(item item) bool {
+	return item.Type == "story" && item.URL != ""
+}
+
+func parseHNItem(hnItem hn.Item) item {
+	ret := item{Item: hnItem}
+	url, err := url.Parse(ret.URL)
+	if err == nil {
+		ret.Host = strings.TrimPrefix(url.Hostname(), "www.")
+	}
+	return ret
+}
+
+// item is the same as the hn.Item, but adds the Host field
+type item struct {
+	hn.Item
+	Host string
+}
+
+type templateData struct {
+	Stories []item
+	Time    time.Duration
+}
