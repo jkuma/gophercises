363141a9ce2fda6b432768c20ad1521331ac9f64
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-11-10 14:25:51 +0100

    Return an ordered list of top stories for package quiet_hn.

--- quiet_hn/main.go
+++ quiet_hn/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/jkuma/gophercises/quiet_hn/hn"
+	"github.com/jkuma/gophercises/quiet_hn/story"
 	"html/template"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
 	"time"
 )
 
@@ -31,26 +29,14 @@ func main() {
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		var client hn.Client
-		ids, err := client.TopItems()
+		stories, err := story.GetTopStories(numStories)
+
 		if err != nil {
-			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
-			return
-		}
-		var stories []item
-		c := make(chan item, numStories)
-		ids = ids[:numStories]
-
-		for _, id := range ids {
-			go getStoryItem(client, id, c)
-		}
-
-		for i := 0; i < numStories; i++ {
-			stories = append(stories, <-c)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		data := templateData{
-			Stories: sort(stories, ids),
+			Stories: stories,
 			Time:    time.Now().Sub(start),
 		}
 		err = tpl.Execute(w, data)
@@ -61,50 +47,7 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	})
 }
 
-func sort(items []item, ids []int) []item {
-	sorted := make([]item, len(items))
-
-	for k, id := range ids {
-		for _, item := range items {
-			if id == item.ID {
-				sorted[k] = item
-			}
-		}
-	}
-
-	return sorted
-}
-
-func getStoryItem(client hn.Client, id int, c chan item) {
-	hnItem, err := client.GetItem(id)
-	if err == nil {
-		item := parseHNItem(hnItem)
-		if isStoryLink(item) {
-			c <- item
-		}
-	}
-}
-
-func isStoryLink(item item) bool {
-	return item.Type == "story" && item.URL != ""
-}
-
-func parseHNItem(hnItem hn.Item) item {
-	ret := item{Item: hnItem}
-	url, err := url.Parse(ret.URL)
-	if err == nil {
-		ret.Host = strings.TrimPrefix(url.Hostname(), "www.")
-	}
-	return ret
-}
-
-// item is the same as the hn.Item, but adds the Host field
-type item struct {
-	hn.Item
-	Host string
-}
-
 type templateData struct {
-	Stories []item
+	Stories []story.Story
 	Time    time.Duration
 }
---
+++ quiet_hn/story/story.go
@@ -0,0 +1,68 @@
+package story
+
+import (
+	"errors"
+	"github.com/jkuma/gophercises/quiet_hn/hn"
+	"net/url"
+	"strings"
+)
+
+// Story is the same as the hn.Item, but adds the Host field
+type Story struct {
+	hn.Item
+	Host string
+}
+
+type result struct {
+	Index int
+	Story Story
+	Error error
+}
+
+func GetTopStories(numStories int) ([]Story, error) {
+	var client hn.Client
+	stories := make([]Story, numStories)
+	ids, err := client.TopItems()
+	if err != nil {
+		return nil, errors.New("failed to load top stories")
+	}
+
+	c := make(chan result, numStories)
+
+	for k, id := range ids[:numStories] {
+		go func(id int, index int) {
+			hnItem, err := client.GetItem(id)
+			if err == nil {
+				Story := parseHNItem(hnItem)
+				if isStoryLink(Story) {
+					c <- result{Index: index, Story: Story}
+					return
+				}
+			}
+
+			c <- result{Error: err, Index: index}
+		}(id, k)
+	}
+
+	for i := 0; i < numStories; i++ {
+		result := <-c
+		if result.Error == nil {
+			stories[result.Index] = result.Story
+		}
+	}
+
+	return stories, nil
+}
+
+func isStoryLink(story Story) bool {
+	return story.Type == "story" && story.URL != ""
+}
+
+func parseHNItem(hnItem hn.Item) Story {
+	ret := Story{Item: hnItem}
+	url2, err := url.Parse(ret.URL)
+	if err == nil {
+		ret.Host = strings.TrimPrefix(url2.Hostname(), "www.")
+	}
+	return ret
+}
