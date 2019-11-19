50358de1453f49cd6f25eae22ac4c9eea95958fe
Jonathan KUMA <jonathan.kuma@gmail.com>
2019-11-19 14:18:37 +0100

    Add embedded types exemple.

---
+++ embeddingtype/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Hello() string
+}
+
+type Cat struct {
+	Color string
+}
+
+func (c Cat) Hello() string {
+	return "Miaaaouuuuu"
+}
+
+type MecanicCat struct {
+	Cat
+}
+
+func catHello(a Animal) {
+	fmt.Println(a.Hello())
+}
+
+func main() {
+	cat := Cat{Color:"black"}
+	catHello(cat)
+
+	mc := MecanicCat{Cat:cat}
+	catHello(mc)
+}
--- recover/main.go
+++ recover/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"runtime/debug"
@@ -32,9 +34,54 @@ func recoverMux(mux *http.ServeMux, develop bool) http.HandlerFunc {
 			}
 		}()
 
-		mux.ServeHTTP(w, r)
+		rw := responseWriter{ResponseWriter: w}
+		mux.ServeHTTP(&rw, r)
 	}
 
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hijacker, ok := rw.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, fmt.Errorf("the ResponseWriter does not support the Hijacker interface")
+	}
+	return hijacker.Hijack()
+}
+
+func (rw *responseWriter) Flush() {
+	flusher, ok := rw.ResponseWriter.(http.Flusher)
+	if !ok {
+		return
+	}
+	flusher.Flush()
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
